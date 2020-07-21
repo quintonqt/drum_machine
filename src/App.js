52d14b811a7803bac5cadcc1fead550ca40c42db
Quinton Tran <quintonqt@gmail.com>
2020-07-21 19:18:22 -0400

    Created containers for drum sound display and drum pads

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div id="drum-machine">
+				<div id="display"></div>
+				<div id="pad-container"></div>
+			</div>
+		);
+	}
 }
 
 export default App;
+
+/* Drum Sounds
+Open HH - https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3
+Closed HH - https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3
+Bass Drum - https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3
+Snare - https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3
+Side-Stick - https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3
+*/
